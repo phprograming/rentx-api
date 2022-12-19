27f72f3aacdfaa9a9e8dc475eef6b2d3a41ebbf4
unknown <paulo_hbs17@hotmail.com>
2022-12-18 21:21:02 -0300

    Alter config

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+import { categoriesRoutes } from './routes/categories.routes';
+
+const app = express();
+
+const port = 3333;
+
+app.use(express.json());
+
+app.use(categoriesRoutes);
+
+
+app.listen(port, () => console.log(`server is running on port ${port}`))
