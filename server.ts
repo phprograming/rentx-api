508886b3367d4b4fd86743dda88dcf8f628309a7
unknown <paulo_hbs17@hotmail.com>
2022-12-19 23:25:55 -0300

    Conclusão de modulo

--- server.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const port = 3333;
-
-const app = express();
-
-app.use(express.json())
-
-app.listen(port, () => console.log(`server is running on port ${port}`))
--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,6 @@ const port = 3333;
 
 app.use(express.json());
 
-app.use(categoriesRoutes);
-
+app.use("/categories",categoriesRoutes);
 
 app.listen(port, () => console.log(`server is running on port ${port}`))
