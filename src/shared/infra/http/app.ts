03ccfd8e9864b030ad3f51d4a8fff911c6497889
Paulo Henrique Barbosa Santos <paulo_hbs17@hotmail.com>
2023-02-03 10:30:41 -0300

    feat: add cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import "dotenv/config";
 import express, { NextFunction, Request, response, Response } from "express";
+import cors from "cors";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 import upload from "@config/upload";
@@ -25,7 +26,9 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
- 
+
+app.use(cors());
+
 app.use(router);
 
 app.use((err: Error, req: Request, res: Response, Next: NextFunction) => {
